5735c6af924df356ee6256488716210f1f56ee2c
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-29 18:22:39 +0700

    feat: add ui for register route

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,37 @@
 <template>
-  <main>
-    <h1> Register </h1>
+  <main id="register">
+    <v-container style="height: 100%">
+      <v-row align="center" justify="center" no-gutters style="height: 100%">
+        <div class="wrapper">
+          <h1 class="mb-8">Register 🐾</h1>
+
+          <v-form>
+            <v-text-field dark label="Username" class="my-2" />
+            <v-text-field dark label="Password" type="password" class="my-2" />
+            <v-text-field dark label="Confirm Password" type="password" class="my-2" />
+
+            <div class="d-flex align-center">
+              <v-btn color="orange" class="my-3" large>Register</v-btn>
+              <v-spacer />
+              <a style="color: orange" @click="$router.push('/login')">Have an account?</a>
+            </div>
+          </v-form>
+        </div>
+      </v-row>
+    </v-container>
   </main>
 </template>
+
+<style lang="scss" scoped>
+#register {
+  background-color: #28282b;
+  height: 100%;
+  color: white;
+
+  .wrapper {
+    margin: 0.5rem;
+    width: 100%;
+    max-width: 600px;
+  }
+}
+</style>
