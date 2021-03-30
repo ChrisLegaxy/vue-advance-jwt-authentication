13f092e81c08736a59a7f4ea2546d69d44898ee0
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-30 21:07:34 +0700

    feat: add a bit of test in

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,14 @@
+<script lang="ts">
+import Vue from 'vue';
+import { getCurrentUser } from '@/api/auth';
+
+export default Vue.extend({
+  async mounted() {
+    await getCurrentUser();
+  },
+});
+</script>
+
 <template>
   <main>
     <h1>
