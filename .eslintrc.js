b65c3d02af3e1e20c6c4297d5476932f86ea9bdb
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-30 20:43:42 +0700

    feat: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-param-reassign': 'off',
     'import/prefer-default-export': 'off',
+    'max-len': 'off',
   },
   overrides: [
     {
