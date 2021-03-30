bebedb9b4b661590daf911c5da393e228880dbea
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-30 17:05:22 +0700

    chore: minor change to eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
@@ -14,6 +14,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-param-reassign': 'off',
+    'import/prefer-default-export': 'off',
   },
   overrides: [
     {
