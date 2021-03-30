e547550408ecc552f6fc13ef20ca43022d30569c
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-30 21:06:32 +0700

    feat: add login functionality to login view with minor changes and linting

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,69 @@
+<script lang="ts">
+import { setAccessToken } from '@/utils/auth';
+import Vue from 'vue';
+import { login } from '@/api/auth';
+
+export default Vue.extend({
+  data: () => ({
+    createUserForm: {
+      username: '',
+      password: '',
+    },
+  }),
+  methods: {
+    async submitLoginForm() {
+      const user = await login(this.createUserForm);
+
+      setAccessToken(user.accessToken);
+
+      this.$router.push('/');
+    },
+  },
+});
+</script>
 <template>
   <main id="login">
     <v-container style="height: 100%">
-      <v-row align="center" justify="center" no-gutters style="height: 100%">
+      <v-row
+        align="center"
+        justify="center"
+        no-gutters
+        style="height: 100%"
+      >
         <div class="wrapper">
-          <h1 class="mb-8">Login 🐾</h1>
+          <h1 class="mb-8">
+            Login 🐾
+          </h1>
 
-          <v-form>
-            <v-text-field dark label="Username" class="my-2" />
-            <v-text-field dark label="Password" type="password" class="my-2" />
+          <v-form @submit.prevent="submitLoginForm">
+            <v-text-field
+              v-model="createUserForm.username"
+              dark
+              label="Username"
+              class="my-2"
+            />
+            <v-text-field
+              v-model="createUserForm.password"
+              dark
+              label="Password"
+              type="password"
+              class="my-2"
+            />
 
             <div class="d-flex align-center">
-              <v-btn color="orange" class="my-3" large>Login</v-btn>
+              <v-btn
+                type="submit"
+                color="orange"
+                class="my-3"
+                large
+              >
+                Login
+              </v-btn>
               <v-spacer />
-              <a style="color: orange" @click="$router.push('/register')">Don't have an account?</a>
+              <a
+                style="color: orange"
+                @click="$router.push('/register')"
+              >Don't have an account?</a>
             </div>
           </v-form>
         </div>
